24479cae3c46d19a0052c8a72234c9e42b04ada4
Omar Bafagih <bafagihomar260@gmail.com>
2022-01-23 23:32:00 -0500

    Changed from request to Axios

--- routers/livegamesRouter.js
+++ routers/livegamesRouter.js
@@ -1,11 +1,15 @@
 //requirements
 const express = require('express');
 const router = express.Router();
-const rp = require('request');
-const url = 'https://sports.yahoo.com/nba/scoreboard/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNhLw&guce_referrer_sig=AQAAAMv4aW3YC3Nw-B69RbCGrJ0xAC8CG68xLLFZK68eXZ8Q5QYq_z7Cg6oaH08MlomBpi9jUP0TWZ0Mz5KsHEx6f-8MqPXguyUUrAyH0FdVAd_jXSt2mLXsYw4FJak_NWzeCX73nsiR08f2AXqSEU5LuhRYvUjXNhAqM_v4NgZm8XuP';
+//const rp = require('request');
+const url = '';
 const cheerio = require('cheerio');
-const request = require('request');
+//const request = require('request');
 const { response } = require('express');
+
+const axios = require("axios")
+//const cheerio = require("cheerio")
+
 
 
 //routing
@@ -16,29 +20,46 @@ function getLiveGames(req, res, next){
     res.format({
         'text/html': ()=>{
             
-            
-            request(url, (error, res, html)=>{
+            async function fetchHTML(url) {
+                const { data } = await axios.get(url)
+                return cheerio.load(data)
+                const $ = await fetchHTML("https://www.espn.com/nba/scoreboard");
+              }
+              
+              
+              
+              
+              // Print the full HTML
+              console.log(`Site HTML: ${$.html()}\n\n`)
+              
+              // Print some specific page content
+              console.log(`First h1 tag: ${$('h1').text()}`)
+            /*request(url, (error, res, html)=>{
+
+                
+                
                 if(!error && res.statusCode == 200){
                     const $ = cheerio.load(html);
-                   // console.log(html);
+                    //console.log(html);
                     let teams = [];
-                    $('.Va(m)').each((i, el)=>{
+                    $('ScoreboardScoreCell pa4 nba ScoreboardScoreCell--post ScoreboardScoreCell--tabletPlus').each((i, el)=>{
                         
                         //const live = $(el).find('.LiveIcon_live__2F3Oq my-1').text();
                         //console.log(live);
-                        //const teamName = $(el).find('.YahooSans Fw(700)! Fz(14px)!').text();
-                        //console.log(teamName);
+                        const teamName = $(el).find('ScoreCell__TeamName ScoreCell__TeamName--shortDisplayName truncate db').text();
+                        console.log(teamName);
                         //if(teamName !== ''){
-                        //    teams.push(teamName)
+                           teams.push(teamName)
                        // }
                         
-                       // console.log(teams);
                         
+                        //console.log(i);
                        
                     });
+                    console.log(teams);
                 
                 }
-            });
+            });*/
 
             res.set('Content-Type', 'text/html');
             res.render('livegames');
