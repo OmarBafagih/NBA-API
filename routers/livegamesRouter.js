d1a3f635aabc51c855607a5a1572c5a966ccf78e
Omar Bafagih <bafagihomar260@gmail.com>
2022-01-24 22:43:47 -0500

    web-scraper fully functioning
    The web-scraper now gets all the live NBA game information from cbssports

--- routers/livegamesRouter.js
+++ routers/livegamesRouter.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio');
 const axios = require("axios")
 //const { response } = require('express');
 
-const url = 'https://www.espn.com/nba/scoreboard';
+const url = 'https://www.cbssports.com/nba/schedule/';
 
 
 //routing
@@ -19,15 +19,28 @@ function getLiveGames(req, res, next){
                 .then(response => {
                     const html = response.data;
                     const $ = cheerio.load(html);
-                    $('.ScoreboardScoreCell__Competitors', html).each(function(){
-                        console.log($(this).text());
-
-                    });
                     //console.log(html);
-                })
-          
-              
-           
+                    const games = [];
+                    const teamNames = [];
+                   $('.CellGame', html).each(function(){
+                        let game = $(this).text().replace(/\s\s+/g, ' ').replace("NBAt", "");
+                        games.push({game});
+                    });
+                    $('.TeamName', html).each(function(){
+                        teamNames.push($(this).text());
+                    });
+                    
+                    let teamcount = 0;
+                    for(let i = 0; i < games.length; i++){
+                        games[i].away = teamNames[teamcount];
+                        teamcount += 1
+                        games[i].home = teamNames[teamcount];
+                        teamcount += 1;
+                    }
+                    console.log(games)
+
+                }).catch(err => console.log(err));
+               ;
             /*request(url, (error, res, html)=>{
 
                 
